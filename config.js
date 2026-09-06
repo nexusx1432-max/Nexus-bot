9491c062b32a0cdffa796e390c237671e2c908dd
nexusx1432-max <nexusx1432@gmail.com>
2026-09-06 11:17:45 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+export const config = {
+  botName: process.env.BOT_NAME || "NEXUS",
+
+  ownerNumber:
+    process.env.OWNER_NUMBER || "628xxxxxxxxxx",
+
+  prefix: ".",
+
+  version: "1.0.0",
+
+  sessionFolder: "./session"
+};
